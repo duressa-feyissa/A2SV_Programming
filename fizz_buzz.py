7508ad432502730cdeb38c12f0f8cd6026d0a677
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2022-09-28 23:48:47 +0300

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -4,14 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        tmp = list()
+        hold = list()
         for i in range(1, n+1):
             if i % 15 == 0:
-                tmp.append("FizzBuzz")
+                hold.append("FizzBuzz")
             elif i % 5 == 0:
-                tmp.append("Buzz")
+                hold.append("Buzz")
             elif i % 3 == 0:
-                tmp.append("Fizz")
+                hold.append("Fizz")
             else:
-                tmp.append(str(i))
-        return tmp
+                hold.append(str(i))
+        return hold
