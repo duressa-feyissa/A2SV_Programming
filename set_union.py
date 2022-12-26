30a1a390fc0bcb8e8a147680dee4c985ea828f19
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2022-12-26 18:47:24 +0300

    Create set_union.py

---
+++ set_union.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+NO_ENGLISH = int(input())
+eng_list = list(map(int, input().split()))
+NO_FRENCH = int(input())
+fre_list = list(map(int, input().split()))
+
+eng_set = set(eng_list)
+fre_set = list(fre_list)
+
+intersection = len(eng_set.intersection(fre_set))
+number = len(eng_set) + len(fre_set) - intersection
+print(number)
