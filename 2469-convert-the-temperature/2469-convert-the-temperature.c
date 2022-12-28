6c0bbe93e9ad65165cf4e7b8243bd13793ae1568
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2022-12-28 04:13:43 +0300

    Time: 3 ms (46.77%), Space: 5.8 MB (28.68%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.c
+++ 2469-convert-the-temperature/2469-convert-the-temperature.c
@@ -1,7 +1,17 @@
-class Solution:
-    def convertTemperature(self, celsius: float) -> List[float]:
-        result = []
-        result.append(celsius + 273.15)
-        result.append(celsius * 1.80 + 32.00)
-        return result
-        
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+double* convertTemperature(double celsius, int* returnSize){
+    *returnSize=2;
+    double *array = (double *)malloc(sizeof(double) * (*returnSize));
+    double k,f;
+
+    celsius*=100000;
+    k=celsius+(273.15*100000);
+    f=celsius*1.8+(32*100000);
+
+    array[0]=k/100000;
+    array[1]=f/100000;
+    
+    return array;
+}
