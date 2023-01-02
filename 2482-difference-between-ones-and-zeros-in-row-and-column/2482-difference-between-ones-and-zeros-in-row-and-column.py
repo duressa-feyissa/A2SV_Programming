92d322cd0381fd5339233cb9df245e912fa32b89
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-01-02 03:29:34 +0300

    Time: 1841 ms (84.20%), Space: 57.4 MB (12.96%) - LeetHub

---
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -0,0 +1,33 @@
+class Solution:
+    def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+        
+        hash_col = {}
+        hash_row = {}
+        
+        for r_index, row in enumerate(grid):
+            count_row_one = 0
+            for c_index, col in enumerate(row):
+                if col == 1:
+                    count_row_one+=1
+                    hash_col[c_index] = 1 + hash_col.get(c_index, 0)
+                else:
+                    hash_col[c_index] = 0 + hash_col.get(c_index, 0)
+                    
+            hash_row[r_index] = count_row_one
+        answer = []
+        row_length = len(grid)
+        col_length = len(grid[0])
+        
+        for i in range(row_length):
+            hold = []
+            for j in range(col_length):
+                x = hash_row[i] + hash_col[j]
+                y = (row_length - hash_row[i]) + (col_length - hash_col[j])         
+                hold.append(x - y)
+            answer.append(hold)
+        return answer
+            
+                    
+                
+                
+        
