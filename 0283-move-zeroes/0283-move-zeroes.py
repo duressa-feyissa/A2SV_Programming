eccb93098697b896eba692f5aa9221da48083d6c
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-01-12 19:13:05 +0300

    Update 0283-move-zeroes.py

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,23 +4,20 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         zeros = nums.count(0)
-        count=0
-        left=0
-        LENGTH = len(nums)
-        while left < LENGTH:
-            while left + count < LENGTH:
-                if nums[left + count] == 0:
-                    count+=1
-                else:
-                    break
-            if left + count < LENGTH:
-                nums[left] = nums[left+count]
-            left+=1
-            
-        left = LENGTH - zeros
-        while left < LENGTH:
-            nums[left] = 0
+        left = 0
+        track = 0
+        while left < len(nums):
+            if nums[left] != 0:
+                nums[track] = nums[left]
+                track+=1
             left+=1
         
+        left = len(nums) - zeros
+        while left < len(nums):
+            nums[left] = 0
+            left += 1
             
+    
+            
+        
         
