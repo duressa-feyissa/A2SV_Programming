fd6816999fba97f4ad4e18f29b5cffa2d914c2c8
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-01-14 23:31:10 +0300

    Time: 215 ms (58.05%), Space: 18.3 MB (8.34%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,45 +1,18 @@
+from collections import defaultdict
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        count = 0
-        m = len(mat)
-        n = len(mat[0])
-        top = 0
-        left = 0
-        right= 0
-        down = 0
+        diagonal = defaultdict(list)
         answer = []
-        while count < m * n:           
-            #increase
-            i = down
-            j = left
-            while (i >= top and j <= right):
-                answer.append(mat[i][j])
-                count += 1
-                i -= 1
-                j += 1
-            if right == n - 1:
-                top += 1
-            if down == m - 1:
-                left += 1 
-            if right < n - 1:
-                right += 1
-            if down < m - 1:
-                down += 1
-            
-            #decrease
-            i = top
-            j = right
-            while (i <= down and j >= left):
-                answer.append(mat[i][j])
-                count += 1
-                i += 1
-                j -= 1
-            if right == n - 1:
-                top += 1
-            if down == m - 1:
-                left += 1
-            if right < n - 1:
-                right += 1
-            if down < m - 1:
-                down += 1 
+        
+        for row in range(len(mat)):
+            for col in range(len(mat[0])):
+                diagonal[row + col].append((row,col))
+        
+        for key in sorted(diagonal.keys()):
+            if key % 2 == 0:
+                temp = sorted(diagonal[key], reverse=True)
+            else:
+                temp = diagonal[key]
+            for item in temp:
+                answer.append(mat[item[0]][item[1]])
         return answer
