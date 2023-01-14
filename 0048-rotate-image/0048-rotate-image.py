6d9b768f606f0a79f0aab9b07625f0726798d145
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-01-15 02:03:09 +0300

    Time: 34 ms (91.01%), Space: 13.9 MB (70.81%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,40 +3,26 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        m = len(matrix)
-        n = len(matrix[0])
-        right = [0,0]
-        down = [0,n - 1]
-        up = [m-1,0]
-        left = [m-1,n-1]
         
-        while True: 
-            if n % 2 != 0:
-                if right[1] == down[1]:
-                    break
-            else:
-                if left[0] + 1 == right[1]:
-                    break
-            while right[1] < down[1]:
-                tmp1 = matrix[right[0]][right[1]]
-                tmp2 = matrix[down[0]][down[1]]
-                matrix[right[0]][right[1]] = matrix[up[0]][up[1]]
-                matrix[down[0]][down[1]] = tmp1
-                matrix[up[0]][up[1]] = matrix[left[0]][left[1]]
-                matrix[left[0]][left[1]] = tmp2
-                right[1] += 1
-                down[0] += 1
-                left[1] -= 1
-                up[0] -= 1
-            right[0] += 1
-            right[1] = right[0]
-            down[0] = right[0]
-            down[1] -= 1
-            left[1] = down[1]
-            left[0] -= 1
-            up[1] = right[1]
-            up[0] = left[0]
-
+        left = 0
+        top = 0
+        right =  len(matrix) - 1
+        bottom = len(matrix) - 1
         
+        while left < right:
+            for i in range(right - left):
+                temp = matrix[top + i][right]
+                matrix[top + i][right] = matrix[top][left + i]
+                matrix[top][left + i] = matrix[bottom - i][left]
+                matrix[bottom - i][left] = matrix[bottom][right - i]
+                matrix[bottom][right - i] = temp
+            left += 1
+            right -= 1
+            top += 1
+            bottom -= 1
+                
+                
+                
+                
             
         
