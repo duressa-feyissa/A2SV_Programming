eb918b0a92d00a0dee2a6874aef5d6de58060bd2
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-01-14 21:30:38 +0300

    Time: 73 ms (50.66%), Space: 13.9 MB (62.45%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,21 +1,19 @@
 from collections import Counter
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        check = Counter(words[0])
+        check = [0] * 26
+        offset = ord('a')
+        for char in words[0]:
+            check[ord(char) - offset] += 1        
+        for word in words:
+            fre = Counter(word)
+            for index in range(len(check)):
+                check[index] = min(fre[chr(index + offset)], check[index])
+                
         answer = []
-        for index in range(1,len(words)):
-            word = Counter(words[index])
-            for key in check.keys():
-                if key in word:
-                    check[key] = min(check[key], word[key])
-                else:
-                    check[key] = -1
-        
-        for key, value in check.items():
-            if value != -1:
-                for i in range(value):
-                    answer.append(key)
-        
+        for val in range(len(check)):
+            if check[val] != 0:
+                answer.extend([chr(val + offset)] * check[val])
         return answer
                 
             
