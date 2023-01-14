fcf236cc6889c222eac59beabb42936f53111f98
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-01-14 23:36:29 +0300

    Time: 200 ms (77.68%), Space: 17.8 MB (46.70%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,3 +1,50 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        count = 0
+        m = len(mat)
+        n = len(mat[0])
+        top = 0
+        left = 0
+        right= 0
+        down = 0
+        answer = []
+        while count < m * n:           
+            #increase
+            i = down
+            j = left
+            while (i >= top and j <= right):
+                answer.append(mat[i][j])
+                count += 1
+                i -= 1
+                j += 1
+            if right == n - 1:
+                top += 1
+            if down == m - 1:
+                left += 1 
+            if right < n - 1:
+                right += 1
+            if down < m - 1:
+                down += 1
+            
+            #decrease
+            i = top
+            j = right
+            while (i <= down and j >= left):
+                answer.append(mat[i][j])
+                count += 1
+                i += 1
+                j -= 1
+            if right == n - 1:
+                top += 1
+            if down == m - 1:
+                left += 1
+            if right < n - 1:
+                right += 1
+            if down < m - 1:
+                down += 1 
+        return answer
+"""
+#methed 2: using map but less efficient
 from collections import defaultdict
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
@@ -16,3 +63,4 @@ class Solution:
             for item in temp:
                 answer.append(mat[item[0]][item[1]])
         return answer
+"""
