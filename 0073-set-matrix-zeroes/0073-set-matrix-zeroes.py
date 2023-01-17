8c738429c0c083e2e44912adbf733b0a736d646f
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-01-17 14:37:08 +0300

    Update 0073-set-matrix-zeroes.py

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,27 +3,35 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        check = matrix[0][0]
-        for row in range(len(matrix)): 
-            for col in range(len(matrix[0])):
-                if row == 0 and matrix[row][col] == 0:
-                    check = 0
-                if row != 0 and matrix[row][col] == 0:
-                    matrix[row][0] = 0
-                    matrix[0][col] = 0
-        for row in range(len(matrix) - 1, 0, -1):
-            if matrix[row][0] == 0:
-                for col in range(len(matrix[0])):
-                    matrix[row][col] = 0
-            else:
-                for col in range(len(matrix[0])):
-                    if matrix[0][col] == 0:
-                        matrix[row][col] = 0
-        if check == 0:
-            for col in range(len(matrix[0])):
-                matrix[0][col] = 0
-            
-                
+        #without using any extra space == O(1)
+        flag = False       
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    if i == 0 and j == 0:
+                        flag = True
+                    else:
+                        if i != 0 and j == 0 and matrix[0][0] == "r":
+                            flag = True
+                        matrix[0][j] = "c"
+                        matrix[i][0] = "r"
+                        
+        #make columns zero
+        for j in range(len(matrix[0])):
+            if matrix[0][j] == "c" or (j == 0 and flag):
+                for i in range(len(matrix)):
+                    if matrix[i][j] != 'r':
+                        matrix[i][j] = 0
+                    
+        #make rows zero
+        for i in range(len(matrix)):
+            if matrix[i][0] == "r" or (i == 0 and flag):
+                for j in range(len(matrix[0])):
+                    matrix[i][j] = 0
+
+        
+                        
+        
+        
                 
         
-       
