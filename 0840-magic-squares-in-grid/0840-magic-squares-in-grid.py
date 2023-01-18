96803ea7ba868f39a7fd3f88c45e887e98a5b42a
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-01-18 06:16:45 +0300

    Time: 44 ms (67.24%), Space: 13.9 MB (92.24%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -12,35 +12,26 @@ class Solution:
         return count
     
     def checker(self, matrix, top, left) -> int:
-        #check row
+        #check elements
+        magic = set([i for i in range(1, 10)])
+        temp = set()
         for i in range(top, top+3):
-            temp = []
             for j in range(left, left+3):
-                if matrix[i][j] > 0 and matrix[i][j] < 10:
-                    temp.append(matrix[i][j])
-            if len(set(temp)) != 3:
-                return 0
-        #check column
-        for i in range(left, left+3):
-            temp = []
-            for j in range(top, top+3):
-                temp.append(matrix[j][i])
-            if len(set(temp)) != 3:
-                return 0
-        #row
+                temp.add(matrix[i][j])
+        if magic != temp:#to check if elements are distinct
+            return 0
+        #add each row
         r1 = matrix[top][left] + matrix[top][left+1] + matrix[top][left+2]
         r2 = matrix[top+1][left] + matrix[top+1][left+1] + matrix[top+1][left+2]
         r3 = matrix[top+2][left] + matrix[top+2][left+1] + matrix[top+2][left+2]
-        #col
+        #add each column
         c1 = matrix[top][left] + matrix[top+1][left] + matrix[top+2][left]
         c2 = matrix[top][left+1] + matrix[top+1][left+1] + matrix[top+2][left+1]
         c3 = matrix[top][left+2] + matrix[top+1][left+2] + matrix[top+2][left+2]
-        #diagonal
+        #add each diagonal
         d1 = matrix[top][left] + matrix[top+1][left+1] + matrix[top+2][left+2]
         d2 = matrix[top+2][left] + matrix[top+1][left+1] + matrix[top][left+2]
         temp = set([r1, r2, r3, c1, c2, c3, d1, d2])
-        print(r1,r2,r3,c1,c2,c3,d1,d2,top,left)
-        print(matrix[top+2][left], matrix[top+1][left+1], matrix[top][left+2])
         if len(temp) == 1:
             return 1
         return 0
