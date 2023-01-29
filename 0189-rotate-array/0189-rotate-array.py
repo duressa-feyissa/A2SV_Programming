44a6a54acf689d623b5fb9d3437eaabc8cf40ee8
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-01-29 11:10:14 +0300

    Time: 236 ms (64.70%), Space: 25.4 MB (76.72%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,13 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        k = k % n
-        array = nums.copy()
-        left = 0
-        right = -k
-        while left < n:
-            nums[left] = array[right]
-            left+=1
-            right+=1
-                
+        n = len(nums) - 1
+        k = k % len(nums)
+        #reverse from index 0 to n - k using two pointer
+        self.reverseArray(nums, 0, n - k)
+        
+        #reverse from index n - k to n using two pointer
+        self.reverseArray(nums, n - k + 1, n)
+        
+        #reverse the whole array using two pointer
+        self.reverseArray(nums, 0, n)
+    
+    @staticmethod
+    def reverseArray(nums, left, right):
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
