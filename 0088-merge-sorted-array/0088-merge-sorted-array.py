d218baf4410057653359f8395e2223f38260bc8d
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-01-29 10:12:44 +0300

    Update 0088-merge-sorted-array.py

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,10 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m
-        j = 0
-        while j < n:
-            nums1[i]=nums2[j]
-            i+=1
-            j+=1
-        nums1.sort()
+        i = m - 1
+        j = n - 1
+        k = m + n -1
+        while j >=0 and i >=0:
+            if nums1[i] <= nums2[j]:
+                nums1[k] = nums2[j]
+                j-=1
+                k-=1
+            else:
+                nums1[k] = nums1[i]
+                i-=1
+                k-=1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j-=1
+            k-=1
