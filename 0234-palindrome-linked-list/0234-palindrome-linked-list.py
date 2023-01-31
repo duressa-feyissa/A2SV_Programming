7ebc9058bd104ab0c91334994ad310c1ac26e39a
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-01-31 19:23:21 +0300

    Time: 696 ms (97.44%), Space: 31 MB (98.73%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,13 +5,34 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+        res = False
+        count = 0
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            count += 1
+        if fast is None:
+             res = Solution.check(slow, head, count)
+        else:
+             res = Solution.check(slow.next, head, count)
+        return res
+    
+    @staticmethod
+    def check(right, head, count):
         node = head
-        data = []
-        while node:
-            data.append(node.val)
+        reverse = None
+        i = 0
+        while i < count:
+            temp = node
             node = node.next
-        reverse = data.copy()
-        reverse.reverse()
-        if data == reverse:
-            return True
-        return False
+            temp.next = reverse
+            reverse = temp
+            i+=1
+        while right:
+            if right.val != reverse.val:
+                return False
+            right = right.next
+            reverse = reverse.next
+        return True
