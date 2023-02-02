f5fa7b38819c8b0c54f95cadeeae60e4bcce17bc
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-02-02 18:40:59 +0300

    Time: 33 ms (87.97%), Space: 13.8 MB (72.54%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -5,40 +5,26 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        smallerNumberHead = None
-        largeNumberHead = None
-        smallNode=None
-        largeNode=None
+        small = ListNode(0, None)
+        large = ListNode(0, None)
         node = head
-        flag1=True
-        flag2=True
+        smallHead = small
+        largeHead = large
         while node:
-            if node.val < x and flag1:
-                smallerNumberHead = node
-                smallNode = node
-                node = node.next
-                smallNode.next = None
-                flag1=False
-            elif node.val >= x and flag2:
-                largeNumberHead = node
-                largeNode = node
-                node = node.next
-                largeNode.next = None
-                flag2=False
-            elif node.val < x and not flag1:
-                smallNode.next = node
-                node = node.next
-                smallNode = smallNode.next
-                smallNode.next = None
-            elif node.val >= x and not flag2:
-                largeNode.next = node
-                node = node.next
-                largeNode = largeNode.next
-                largeNode.next = None
-        if smallNode:
-            smallNode.next = largeNumberHead
-            return smallerNumberHead
-        return largeNumberHead
+            if node.val < x:
+                small.next = node
+                small = small.next
+            else:
+                large.next = node
+                large = large.next
+            node = node.next
+        large.next = None
+        small.next = largeHead.next
+        
+        return smallHead.next
+        
+        
+        
         
         
         
