4267b2258a63a719aafd02fb32516a7887385082
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-02-19 19:44:16 +0300

    Update B_Dakti.py

--- Contest/B_Dakti.py
+++ Contest/B_Dakti.py
@@ -20,3 +20,24 @@ for i in range(t):
     tmp.sort()
     a = [hash_map[i] for i in tmp]
     print(" ".join(a))
+
+"""
+no = int(input())
+check = set([str(i) for i in range(0,10)])
+for _ in range(no):
+    words = input().split()
+    hash_word = {}
+    for word in words:
+        num = ""
+        temp = ""
+        for c in word:
+            if c in check:
+                num += c
+            else:
+                temp += c
+        hash_word[int(num)] = temp
+    ans = []
+    for k in sorted(hash_word.keys()):
+        ans.append(hash_word[k])
+    print(*ans)
+"""
