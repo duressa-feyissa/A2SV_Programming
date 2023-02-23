d74e846dcd2f6721a28c28201600992862d358d7
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-02-24 00:47:51 +0300

    Time: 328 ms (18.99%), Space: 18.9 MB (15.98%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,13 +1,13 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        remenderFrequency = [0]*k
-        add = 0
-        for x in nums:
-            add += x%k
-            remenderFrequency[add%k] += 1
-        answer = remenderFrequency[0]
-        for c in remenderFrequency:
-            answer += (c * (c - 1))//2
-        return answer
+        hash_map = {0 : 1}
+        total = 0
+        ans = 0
+        for num in nums:
+            total += num
+            ans += hash_map.get(total % k, 0)
+            hash_map[total % k] = hash_map.get(total % k, 0) + 1   
+        return ans
+        
 
         
