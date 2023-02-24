d6b77c7b62724663a2b45f4123a02e8a6148b2ce
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-02-24 03:44:01 +0300

    Time: 72 ms (82.75%), Space: 14.3 MB (80.59%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -3,15 +3,14 @@ class Solution:
         maxDistance = 0
         for trip in trips:
             maxDistance = max(maxDistance, trip[2])
-        
         maxDistance += 1
         
         array = [0] * (maxDistance)
-        for trip in trips:
-            if trip[0] > capacity:
+        for passengers, start, end in trips:
+            if passengers > capacity:
                 return False
-            array[trip[1]] += trip[0]
-            array[trip[2]] -= trip[0]
+            array[start] += passengers
+            array[end] -= passengers
 
         for i in range(1, maxDistance):
             array[i] += array[i - 1]
