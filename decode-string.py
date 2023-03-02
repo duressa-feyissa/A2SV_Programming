8dd1db6547f38846c21767fc90c128e7dd9b2d82
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-03-02 19:43:10 +0300

    added decode-string.python

---
+++ decode-string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        return self.solve(s)[0]
+    
+    def solve(self, s):
+        digit = ""
+        char = ""
+        hold = ""
+        
+        if s == "":
+            return ["", ""]
+        
+        if s[0] == "]":
+            return ["", s[1:]]
+
+        while s and s[0].isdigit():
+            digit += s[0]
+            s = s[1:]
+
+        substrings = self.solve(s[1:])
+        
+        if s and s[0] != "[":
+            char = s[0] + substrings[0]
+        else:
+            char = substrings[0]
+        if digit:
+            temp = self.solve(substrings[1])
+            return [(int(digit) * char) + temp[0], temp[1]] 
+
+        return [char, substrings[1]]
