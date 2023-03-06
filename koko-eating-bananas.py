11e7b844449f83a6a7465b9dcc41b50a2dfd552b
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-03-06 17:23:24 +0300

    added koko-eating-bananas.python

---
+++ koko-eating-bananas.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        left = 1
+        right = max(piles)
+        valid = float('inf')
+        middle = 0
+        while left <= right:
+            
+            middle = (left + right) // 2
+            
+            time = 0
+            for num in piles:
+                time += ceil(num/middle)
+
+            if time <= h:
+                valid = middle
+                right = middle - 1
+            else:
+                left = middle + 1
+        
+        return valid
