8ca6a0189926d59221ecbdfeb2a3f53e04504190
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-03-08 15:32:19 +0300

    Time: 78 ms (95.15%), Space: 15.2 MB (10.55%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        subarray = []
+        array = []
+        self.subarrayPreOrderTraversal(subRoot, subarray)
+        self.PreOrderTraversal(root, array)
+        return  " ".join(subarray) in " ".join(array)
+        
+    def PreOrderTraversal(self, root, array):
+        if root == None:
+            array.append(str(None))
+            return
+        array.append(str("[" + str(root.val) + "]"))
+        self.PreOrderTraversal(root.left, array)
+        self.PreOrderTraversal(root.right, array) 
+        
+    def subarrayPreOrderTraversal(self, root, subarray):
+        if root == None:
+            subarray.append(str(None))
+            return
+        subarray.append("[" + str(root.val) + "]")
+        self.subarrayPreOrderTraversal(root.left, subarray)
+        self.subarrayPreOrderTraversal(root.right, subarray)
+        
+
+        
