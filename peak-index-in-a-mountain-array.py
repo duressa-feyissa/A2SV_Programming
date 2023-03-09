6929fde3edeb95216922f3663264008ec43a9c1a
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-03-09 18:36:15 +0300

    added peak-index-in-a-mountain-array.python

---
+++ peak-index-in-a-mountain-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        
+        left = 0
+        n = len(arr)
+        right = n - 1
+        
+        while left <= right:
+            
+            middle = (right + left) // 2
+            
+            if middle == 0:
+                left = middle + 1
+            elif middle == n - 1:
+                right = middle - 1
+            elif arr[middle] > arr[middle + 1] and arr[middle] > arr[middle - 1]:
+                return middle
+            elif arr[middle] < arr[middle + 1] and arr[middle] > arr[middle - 1]:
+                left = middle + 1
+            elif arr[middle] > arr[middle + 1] and arr[middle] < arr[middle - 1]:
+                right = middle - 1
