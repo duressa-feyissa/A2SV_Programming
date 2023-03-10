8e2272696e99cfefd580f85216820d0fd6f55336
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-03-10 10:45:25 +0300

    added merge-two-binary-trees.python

--- merge-two-binary-trees.py
+++ merge-two-binary-trees.py
@@ -5,14 +5,11 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        
-        def helper(root1, root2):
-            if root1 == None and root2 == None:
-                return None
-            if root1 == None:
-                root1 = TreeNode()    
-            if root2 == None:
-                root2 = TreeNode()
-            return TreeNode(root1.val + root2.val, helper(root1.left, root2.left), helper(root1.right, root2.right))
-        return helper(root1, root2)
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:   
+        if root1 == None and root2 == None:
+            return None
+        if root1 == None:
+            root1 = TreeNode()    
+        if root2 == None:
+            root2 = TreeNode()
+        return TreeNode(root1.val + root2.val, self.mergeTrees(root1.left, root2.left), self.mergeTrees(root1.right, root2.right))
