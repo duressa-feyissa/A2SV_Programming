2d2cd67c28f970ed95468d722c4019a8b4bd441c
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-03-13 11:58:51 +0300

    added combinations.python

---
+++ combinations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        answer = []
+
+        def backTrack(i, combination):
+            if len(combination) == k:
+                answer.append(combination[:])
+                return
+            
+            if i == n:
+                return
+            
+            for j in range(i, n):
+                backTrack(j + 1, combination + [j + 1])
+
+        backTrack(0, [])    
+        return answer
