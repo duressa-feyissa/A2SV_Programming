15f3377c9ddee81f0ce3be06196c9839db8c6680
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-04-09 15:12:55 +0300

    Time: 115 ms (36.07%), Space: 15.6 MB (99.04%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        array = [0] * 32
+        sign = 0
+        for num in nums:
+            if num < 0:
+                sign += 1
+                num = num * -1
+            i = 0
+            while num:
+                if num & 1:
+                    array[i] += 1
+                num = num >> 1
+                i += 1
+        answer = 0
+        for i in range(32):
+            if array[i] % 3:
+                answer |= (1 << i)
+        if sign % 3 != 0:
+            answer = answer * -1
+        return answer
+        
+        
+        
