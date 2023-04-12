d3aa7de8fa18b66644e678eff2917fc55c99bcc3
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-04-12 19:44:38 +0300

    added all-paths-from-source-to-target.python

---
+++ all-paths-from-source-to-target.py
@@ -0,0 +1,18 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        adjacent = defaultdict(list)
+        n = len(graph)
+        for i in range(n):
+            adjacent[i].extend(graph[i])
+        answer = []
+        
+        def dfs(current, path):
+            if current == n - 1:
+                answer.append(path[:])
+                return
+            for i in adjacent[current]:
+                path.append(i)
+                dfs(i, path)
+                path.pop()
+        dfs(0, [0])
+        return answer
