be25d77b4db53336f980441067c8881f3e3f691b
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-07-02 15:50:11 +0300

    Time: 1607 ms (83.95%), Space: 62.7 MB (47.68%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,16 +1,8 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        memory = {}
         N = len(questions)
-        def dp(index):
-            if index >= N:
-                return 0
-            if index not in memory:
-                memory[index] = max(dp(index + questions[index][1] + 1) + questions[index][0], dp(index + 1))
-            return memory[index]
-        return dp(0)
-    
-                    
-            
-            
-        
+        Max = max(list(map(lambda x: x[1], questions)))
+        memory = [0] * (Max + 10 + N)
+        for i in range(N - 1, -1, -1):
+            memory[i] = max(memory[i + questions[i][1] + 1] + questions[i][0], memory[i + 1])
+        return memory[0]
