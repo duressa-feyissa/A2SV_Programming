246828b0fd00ac3a2cca6aa7c74789f0e3e69297
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-07-13 10:56:09 +0300

    Time: 77 ms (6.91%), Space: 16.9 MB (16.66%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -1,28 +1,41 @@
 class Solution:
-    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
 
-        def add_parent(cur, parent):
-            if cur:
-                cur.parent = parent
-                add_parent(cur.left, cur)
-                add_parent(cur.right, cur) 
-                
-        add_parent(root, None)  
-        queue = deque([target])
-        visited = set()
-        answer = []
-        while queue and k >= 0:
-            N = len(queue)
-            for _ in range(N):
-                popped = queue.popleft()
-                if not popped or popped in visited:
-                    continue
-                visited.add(popped)
-                if k == 0:
-                    answer.append(popped.val)
-                queue.append(popped.left)
-                queue.append(popped.right)
-                queue.append(popped.parent)
-            k -= 1
+    def __init__(self):
+        self.result = []
+        self.back = []
+        self.check = set()
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        self.helper(root, target.val, k, [])
+        self.check.add(target.val)
+        n = len(self.back)
+        print(self.back)
+        for i in range(n - 1, -1, -1):
+            self.search(self.back[i], k - (n - i))
+        return self.result
+
+    def helper(self, root, target, k, stack):
+        if root == None:
+            return
+        stack.append(root)
+        self.helper(root.left, target, k, stack)
+        self.helper(root.right, target, k, stack)
+        if len(stack) > k and stack[-(k + 1)].val == target:
+            self.result.append(root.val)
+        stack.pop()
+        if root.val == target:
+            self.back = stack.copy()
+
+    def search(self, root, k):
+        if root == None:
+            return
+        if root.val in self.check:
+            return
+        if k == 0:
+            self.check.add(root.val)
+            self.result.append(root.val)
+            return
+        self.search(root.left, k - 1)
+        self.search(root.right, k - 1)
+        self.check.add(root.val)
         
-        return answer
+        
