e97057b3d069f1e025ae93d2eefca3019d1e3d18
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-07-18 09:34:16 +0300

    Time: 165 ms (40.25%), Space: 17.2 MB (78.92%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,24 +1,20 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         N = len(matrix)
-        memory = {}
-        def inbound(r, c):
-            return 0 <= c < N
-        dirs = [(1,0), (1,1), (1,-1)]
-        def dp(r, c):
-            if r > N - 1:
-                return 0
-            state = (r, c)
-            if state in memory:
-                return memory[state]
-            Min = float('inf')
-            for x, y in dirs:
-                nr, nc = x + r, y + c
-                if inbound(nr, nc):
-                    Min = min(Min, dp(nr, nc) + matrix[r][c])
-            memory[state] = Min
-            return Min
-        Min = float('inf')
-        for i in range(N):
-            Min = min(dp(0,i), Min)
-        return Min
+        memory = [0] * N
+        for i in range(N-1,-1,-1):
+            next = [0] * N
+            for j in range(N):
+                Min = float('inf')
+                if j - 1 > -1:
+                    Min = min(memory[j - 1], Min)
+                if j + 1 < N:
+                    Min = min(memory[j + 1], Min)
+                Min = min(memory[j], Min)
+                next[j] = Min + matrix[i][j]
+            memory = next
+        return min(memory)
+                    
+                    
+                    
+                
