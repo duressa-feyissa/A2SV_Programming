08fd4a342047ac7b73ee18315a3b1d8d5250165f
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-08-12 20:49:31 +0300

    Time: 76 ms (50.13%), Space: 19.4 MB (7.16%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,19 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        memory = {}
         N = len(prices)
-        def dp(index, stat):
-            if index > N - 1:
-                return 0
-            state = (index, stat)
-            if state in memory:
-                return memory[state] 
-            Max = 0
-            if stat == 'B':
-                Max = max(dp(index + 1, 'B'), dp(index + 1, 'S') - prices[index])
-            else:
-                Max = max(dp(index + 1, 'B') + prices[index], dp(index + 1, 'S'))
-            memory[state] = Max
-            return Max
-        return dp(0, 'B')
-                
+        memory = [[0,0] for _ in range(N+1)]
+        for i in range(N-1, -1, -1):
+            memory[i][0] = max(memory[i+1][1] - prices[i], memory[i+1][0])
+            memory[i][1] = max(memory[i+1][0] + prices[i], memory[i+1][1])
+        return memory[0][0]
+
+        
