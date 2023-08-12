bd56e9d7a3f4700025fe4db7cfe98a749ea9081f
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-08-12 20:51:15 +0300

    Time: 68 ms (77.39%), Space: 17.7 MB (74.64%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,10 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         N = len(prices)
-        memory = [[0,0] for _ in range(N+1)]
+        hold, free = 0, 0
         for i in range(N-1, -1, -1):
-            memory[i][0] = max(memory[i+1][1] - prices[i], memory[i+1][0])
-            memory[i][1] = max(memory[i+1][0] + prices[i], memory[i+1][1])
-        return memory[0][0]
-
-        
+            hold = max(free - prices[i], hold)
+            free = max(hold + prices[i], free)
+        return hold        
