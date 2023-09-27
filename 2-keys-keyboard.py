181310cca713e4cf199ff577ac3c920dc6ae85f5
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-09-28 02:26:19 +0300

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -1,11 +1,13 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        count = 0
-        dividor = 2
-        while n != 1:
-            if n % dividor == 0:
-                n = n // dividor
-                count += dividor
-            else:
-                dividor += 1
-        return count
+        memory = [-1] * (n + 1)
+        for i in range(1,n+1):
+            found = False
+            for j in range(i-1, 0,-1):
+                if memory[j] != -1 and i % j == 0:
+                    memory[i]= (i // j) + memory[j]
+                    found = True
+                    break
+            if not found:
+                memory[i] = i
+        return memory[n] - 1
