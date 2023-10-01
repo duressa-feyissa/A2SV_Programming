2104abd2c0333c245c656feeed5009a4f0cf4444
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-10-01 11:27:43 +0300

    Time: 1524 ms (35.33%), Space: 16.3 MB (99.55%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -5,25 +5,10 @@ class Solution:
         for i in range(N):
             array.append((ages[i], scores[i]))
         array.sort()
-        array = [(0, 0)] + array
-        # memory = {}
-        # def dp(index, maxIndex):
-        #     if index > N:
-        #         return 0
-        #     state = (index, maxIndex)
-        #     if state in memory:
-        #         return memory[state]
-        #     include = 0
-        #     exclude = dp(index + 1, maxIndex)
-        #     if array[index][1] >= array[maxIndex][1]:
-        #         include = dp(index + 1, index) + array[index][1]
-        #     memory[state] = max(include, exclude)
-        #     return memory[state]
-        # return dp(0, 0)
-    
-        
-        dp = [0] * (N + 1)
-        for i in range(1, N + 1):
+        dp = []
+        for i in range(N):
+            dp.append(array[i][1])
+        for i in range(1, N):
             for j in range(i):
                 if array[j][1] <= array[i][1]:
                     dp[i] = max(dp[i], dp[j] + array[i][1])
