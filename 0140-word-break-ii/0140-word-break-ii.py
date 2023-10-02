11dfc7c2e026ceed6bdcfb2a9e7388f595a3d1b2
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-10-02 17:09:23 +0300

    Time: 43 ms (21.68%), Space: 16.4 MB (13.47%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,21 +1,55 @@
+class TrieNode:
+    
+    def __init__(self):
+        self.children = [None] * 26
+        self.index = 0
+        
+class Trie:
+    
+    def __init__(self):
+        self.root = TrieNode()
+        self.N = 0
+        
+    def insert(self, word):
+        current = self.root
+        for i, c in enumerate(word):
+            index = ord(c) - ord('a')
+            if not current.children[index]:
+                current.children[index] = TrieNode()
+            current.index = i
+            current = current.children[index]
+        
+    def helper(self, word, wordDict, pointer):
+        result = []
+        n = len(word)
+        for i, w in enumerate(word):
+            index = ord(w) - ord('a')
+            if not pointer.children[index] or  pointer.index > self.N:
+                return [[None, False]]
+            if pointer.index == self.N - 1 and i == n -1:
+                return [[word, True]]
+            pointer = pointer.children[index]
+        if pointer.index > self.N:
+            return [[None, False]]
+        for wrd in wordDict:
+            sResult = self.helper(wrd, wordDict, pointer)
+            for val in sResult:
+                if val[1]:
+                    result.append([word + " " + val[0], val[1]])
+        return result
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        N = len(s)
-        answer = []
-        def backTrack(index, path):
-            if index >= N:
-                answer.append(" ".join(path[:]))
-                return
-            for word in wordDict:
-                if word == s[index: len(word) + index]:
-                    path.append(word)
-                    temp = backTrack(index + len(word), path)
-                    path.pop()
-                    
-        backTrack(0, [])
-        
-        return answer
-                    
-        
-        
+        trie = Trie()
+        trie.insert(s)
+        trie.N = len(s)
+        result = []
+        current = trie.root
+        for word in wordDict:
+            result.extend(trie.helper(word, wordDict, current))
+        ans = []
+        for pos in result:
+            if pos[1]:
+                ans.append(pos[0])        
+        return ans
         
