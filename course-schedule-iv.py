517e3564ef73199f472238d1a0978b9fc5b561b1
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-10-04 10:01:59 +0300

    added course-schedule-iv.python

--- course-schedule-iv.py
+++ course-schedule-iv.py
@@ -1,30 +1,19 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        distance = [[float('inf')] * numCourses for _ in range(numCourses)]
 
-        indegrees = defaultdict(int)
-        graph = defaultdict(list)
-        dependecies = defaultdict(set)
-        N = len(prerequisites)
-        for i in range(N):
-            indegrees[prerequisites[i][1]] += 1
-            graph[prerequisites[i][0]].append(prerequisites[i][1])
-        
-        queue = deque([])
-        for key in range(numCourses):
-            if indegrees[key] == 0:
-                queue.append(key)
-        while queue:
-            popped = queue.popleft()
-            for node in graph[popped]:
-                indegrees[node] -= 1
-                dependecies[node].update(dependecies[popped])
-                dependecies[node].add(popped)
-                if indegrees[node] == 0:
-                    queue.append(node)
+        for i, j in prerequisites:
+            distance[i][j] = 1
+
+        for i in range(numCourses):
+            distance[i][i] = 0
+
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+
         answer = []
-        for x, y in queries:
-            if x in dependecies[y]:
-                answer.append(True)
-            else:
-                answer.append(False)
+        for start, end in queries:
+                answer.append(distance[start][end] != float('inf'))
         return answer
