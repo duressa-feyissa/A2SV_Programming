21e0b556e237bfada967255bc21dfa93b10bbf34
Duresa Feyisa <101529984+duressa-feyissa@users.noreply.github.com>
2024-03-24 08:00:53 +0300

    Time: 1294 ms (5.07%) | Memory: 30.9 MB (32.41%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        N = len(nums)
-        for i in range(N):
-            while nums[i] != i + 1:
-                correctPosition = nums[i] - 1
-                if nums[i] == nums[correctPosition]:
-                    return nums[i]
-                nums[correctPosition], nums[i] = nums[i], nums[correctPosition]
+        bit_vector = 0
+        
+        for num in nums:
+            bit_mask = 1 << (num - 1)
+            if bit_vector & bit_mask:
+                return num
+            bit_vector |= bit_mask
